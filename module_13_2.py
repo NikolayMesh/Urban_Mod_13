7a59d90549f1e0c88a167f550e5a86a2d97664d7
NikolayMesh <partizan.91@bk.ru>
2024-12-09 17:39:16 +0300

    add

---
+++ module_13_2.py
@@ -0,0 +1,25 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+import data1
+
+
+bot = Bot(token = data1.api)
+dp = Dispatcher(bot, storage = MemoryStorage())
+
+
+@dp.message_handler(text =['ss', 'ff'])
+async def urban_message(message):
+    print("Urban message")
+
+@dp.message_handler(commands =['start'])
+async def start_message(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_message(message):
+    print("Введите команду /start, чтобы начать общение.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates = True)
+
