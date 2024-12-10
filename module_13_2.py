782409ca43e08c0d94e5d1a38e03b0925bf1a54d
NikolayMesh <partizan.91@bk.ru>
2024-12-10 11:19:03 +0300

    add+

--- module_13_2.py
+++ module_13_2.py
@@ -11,14 +11,17 @@ dp = Dispatcher(bot, storage = MemoryStorage())
 @dp.message_handler(text =['ss', 'ff'])
 async def urban_message(message):
     print("Urban message")
+    await message.answer("Привет!")
 
 @dp.message_handler(commands =['start'])
 async def start_message(message):
     print("Привет! Я бот помогающий твоему здоровью.")
+    await message.answer("Рады видеть Вас у Нас! :) ")
 
 @dp.message_handler()
 async def all_message(message):
     print("Введите команду /start, чтобы начать общение.")
+    await message.answer(message.text)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates = True)
